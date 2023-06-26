189abf155000b0acbe46bc80a500fd9d73b54030
harshmunjapara09 <harshmunjapara005@gmail.com>
2023-06-26 17:21:31 +0530

    26/06/2023

--- Recursion/Basics.java
+++ Recursion/Basics.java
@@ -1,6 +1,7 @@
 package Recursion;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Basics {
     public static void main(String[] args) {
@@ -37,28 +38,147 @@ public class Basics {
 //        System.out.println(lastOccurence1(arr,key,0));
 
 
-        int n = 2;
-        int power = 10;
-//        One Way
-        System.out.println(findPower(n, power, 1));
-//        Secound Way
-        System.out.println(findPower1(n, power));
-//        Third Way Optimize
-        System.out.println(optimizeFindPower(n,power));
+//        int n = 2;
+//        int power = 10;
+////        One Way
+//        System.out.println(findPower(n, power, 1));
+////        Secound Way
+//        System.out.println(findPower1(n, power));
+////        Third Way Optimize
+//        System.out.println(optimizeFindPower(n,power));
+
+//        int n= 5;
+//        System.out.println(tiling(n));
+
+//        int[] arr = {1, 2, 3, 4, 5, 2, 5, 6, 2, 2, 6, 6};
+//        int key = 2;
+//        ArrayList<Integer> list = new ArrayList<>();
+//        System.out.println(findKeyIndex(arr, key, 0, list));
+
+//        int n = 982002;
+//        StringBuilder s = new StringBuilder();
+//        System.out.println(numberConvertString(n,s));
+
+        String s = "Harsh";
+        System.out.println(findlength(s));
 
 
     }
 
-    public static int optimizeFindPower(int n, int power) {
-        if (power==0){
+    public static int findlength(String s) {
+
+        return 0;
+    }
+
+    private static String numberConvertString(int n,StringBuilder s) {
+        if (n==0){
+            s.reverse();
+            return "";
+        }
+        int last = n % 10;
+        switch (last){
+            case 0 :
+                s.append("zero ");
+                break;
+
+            case 1 :
+                s.append("one ");
+                break;
+
+            case 2 :
+                s.append("two ");
+                break;
+
+            case 3 :
+                s.append("three ");
+                break;
+
+            case 4 :
+                s.append("four ");
+                break;
+
+            case 5 :
+                s.append("five ");
+                break;
+
+            case 6 :
+                s.append("six ");
+                break;
+
+            case 7 :
+                s.append("seven ");
+                break;
+
+            case 8 :
+                s.append("eight ");
+                break;
+
+            case 9 :
+                s.append("nine ");
+                break;
+
+        }
+        n = n/10;
+        numberConvertString(n,s);
+        StringBuilder s11 = new StringBuilder();
+        String [] arr = s.toString().split(" ");
+        for (String s1 : arr){
+            s11.append(replace(s1)).append(" ");
+        }
+        return s11.toString().trim();
+    }
+
+    private static String replace(String s1) {
+        char [] arr = s1.toCharArray();
+        StringBuilder s = new StringBuilder();
+        int left =0;
+        int right= s1.length()-1;
+        while (left < right){
+            char temp = arr[left];
+            arr[left]= arr[right];
+            arr[right]=temp;
+            left++;
+            right--;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            s.append(arr[i]);
+        }
+        return s.toString();
+    }
+
+    public static List<Integer> findKeyIndex(int[] arr, int key, int i, ArrayList<Integer> list) {
+        if (i == arr.length) {
+            return list;
+        }
+        if (arr[i] == key) {
+            list.add(i);
+        }
+        findKeyIndex(arr, key, i + 1, list);
+        return list;
+    }
+
+    public static int tiling(int n) {
+        if (n == 0 || n == 1) {
             return 1;
         }
-        int halfpower = optimizeFindPower(n,power/2) * optimizeFindPower(n,power/2);
+        int fnm1 = tiling(n - 1);
+        int fnm2 = tiling(n - 2);
 
-        if (power%2!=0){
-            halfpower = 2* halfpower;
+        int total = fnm1 + fnm2;
+        return total;
+    }
+
+    public static int optimizeFindPower(int n, int power) {
+        if (power == 0) {
+            return 1;
         }
-        return halfpower;
+        int halfpower = optimizeFindPower(n, power / 2);
+        int fullpower = halfpower * halfpower;
+
+        if (power % 2 != 0) {
+            fullpower = n * fullpower;
+        }
+        return fullpower;
     }
 
     public static int findPower1(int n, int power) {
