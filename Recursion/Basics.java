735b2a9c2b0615e5bd0021276b2663b2f407e5f2
harshmunjapara09 <harshmunjapara005@gmail.com>
2023-06-30 11:10:48 +0530

    30/06/2023

--- Recursion/Basics.java
+++ Recursion/Basics.java
@@ -1,6 +1,7 @@
 package Recursion;
 
 import java.util.ArrayList;
+import java.util.IllegalFormatCodePointException;
 import java.util.List;
 
 public class Basics {
@@ -58,9 +59,29 @@ public class Basics {
 //        StringBuilder s = new StringBuilder();
 //        System.out.println(numberConvertString(n,s));
 
-        int n=3;
-        System.out.println(friendPairing(n));
+//        int n=3;
+//        System.out.println(friendPairing(n));
 
+
+        int [] arr ={1,2,3,4,5,6,7,8};
+        int target = 12;
+        System.out.println(Search(arr,target,0,arr.length-1));
+
+    }
+
+    public static int Search(int[] arr,int target,int si,int ei) {
+        if (si > ei){
+            return -1;
+        }
+        int mid = si + (ei - si) / 2;
+        if (arr[mid]==target){
+            return mid;
+        }
+
+        if (target < arr[mid]){
+            return Search(arr,target,si,mid-1);
+        }
+        return Search(arr,target,mid+1,ei);
     }
 
     private static int friendPairing(int n) {
