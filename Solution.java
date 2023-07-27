1e2c40223f99c2656b49a549c99a5abf49e235f6
harshmunjapara09 <harshmunjapara005@gmail.com>
2023-07-27 11:22:21 +0530

    27/07/2023

--- Solution.java
+++ Solution.java
@@ -1,9 +1,25 @@
-////class Solution {
-////    public ListNode sortList(ListNode head) {
-////
-////    }
-////}
-//class ListNode{
-//    int data;
+//
+//public class Solution {
+//    public ListNode oddEvenList(ListNode head) {
+//        ListNode slow = head;
+//        ListNode fast = head.next;
+//
+//    }
+//}
+//
+//class ListNode {
+//    int val;
 //    ListNode next;
+//
+//    ListNode() {
+//    }
+//
+//    ListNode(int val) {
+//        this.val = val;
+//    }
+//
+//    ListNode(int val, ListNode next) {
+//        this.val = val;
+//        this.next = next;
+//    }
 //}
