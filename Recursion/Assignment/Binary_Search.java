8a7253a8d62508ed998c4b7bcafcb888f5dca021
harshmunjapara09 <harshmunjapara005@gmail.com>
2023-08-11 16:56:08 +0530

    11/08/2023

---
+++ Recursion/Assignment/Binary_Search.java
@@ -0,0 +1,26 @@
+package Recursion.Assignment;
+
+public class Binary_Search {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int target = 5;
+        System.out.println(findIndex(arr, target, 0, arr.length - 1));
+
+    }
+
+    private static int findIndex(int[] arr, int target, int s, int e) {
+        if (s > e) {
+            return -1;
+        }
+
+        int mid = s + (e - s) / 2;
+        if (arr[mid]==target){
+            return mid;
+        }
+
+        if (arr[mid] > target){
+            return findIndex(arr,target,s,mid-1);
+        }
+        return findIndex(arr,target,mid+1,e);
+    }
+}
